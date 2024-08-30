1ef47e41f87a4aa1cb0e50f8e7a5317b8e2b470e
ShreyaD2001 <shreya.deshmukh@prorigosoftware.com>
2024-08-30 18:55:17 +0530

    Add files via upload
    auth user branch

---
+++ HomeControllerTests.cs
@@ -0,0 +1,44 @@
+﻿using AuthUser.Controllers;
+using AuthUser.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+
+
+namespace AuthUser.Tests
+{
+    [TestClass]
+    public class HomeControllerTests
+    {
+        private HomeController _controller;
+        private Mock<ILogger<HomeController>> _mockLogger;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _mockLogger = new Mock<ILogger<HomeController>>();
+            _controller = new HomeController(_mockLogger.Object);
+        }
+
+        [TestMethod]
+        public void Index_ReturnsAViewResult()
+        {
+            // Act
+            var result = _controller.Index() as ViewResult;
+
+         
+            Assert.IsNotNull(result);
+            // Adjust the expected view name as needed
+            Assert.AreEqual("Index", result.ViewName);
+        }
+
+   
+    }
+}
+
+
+
+
+
